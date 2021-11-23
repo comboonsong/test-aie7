b3c88a4f1dcf0b2c780c98d09e5420d3e826566c
comboonsong <78197856+comboonsong@users.noreply.github.com>
2021-11-23 16:33:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,4 @@ app = flask.Flask(__name__)
 @app.route("/")
 def test():
     data = {'items':['bottle','cup'],'count':{'bottle':1,'cup':2}}
-    print(data)
     return data
-
-if __name__ == "__main__":
-    test()
