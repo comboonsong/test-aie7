82cfc52fedb0257d23bc335ee59c6a111ab88015
comboonsong <78197856+comboonsong@users.noreply.github.com>
2021-11-25 13:45:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-import flask
-app = flask.Flask(__name__)
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
 
 @app.route("/")
 def index():
-    data = {'items':['bottle','cup'],'count':{'bottle':1,'cup':2}}
-    return data
-if __name__ == '__main__':
+    return "Hi!"
+
+@app.route('/items')
+def data1():
+    #data = {'items':['bottle','cup'],'count':{'bottle':1,'cup':2}}
+    items = {'items':['bottle','cup']}
+    return jsonify(items)
+
+@app.route('/count')
+def data2():
+    #data = {'items':['bottle','cup'],'count':{'bottle':1,'cup':2}}
+    count = {'count': ['bottle=1','cup=2']}
+    return jsonify(count)
+
+if  __name__ == '__main__':
     app.run(threaded=True)
